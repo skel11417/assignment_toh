c65d2b3ba9cb4d623af698e03eec8087bb9c01e1
Sean Kelly <shonkelli@gmail.com>
2017-03-26 14:09:02 -0400

    Have not worked out all the bugs, but the most important part of the assignment is done, I know how to fix everything else

--- toh.rb
+++ toh.rb
@@ -3,20 +3,19 @@
 # Enter where you'd like to move from and to
 # in the format '1,3'. Enter 'q' to quit.
 
-def finish_game(rods, num_rings)
+def finish_game(rods, num_rings, game_on)
   if (1..num_rings).to_a == rods[1] or (1..num_rings).to_a == rods[2]
     puts "You win"
-    puts rods[0].to_s
-    puts rods[1].to_s
-    puts rods[2].to_s
-    #exit
+    game_on = false
+  elsif
+  puts game_on
   end
 end
 
-def current_board(rods)
+def render(rods, num_rings)
   board_output_string = "            "
   puts "Current Board:"
-  (0..2).to_a.each do |line|
+  (0..(num_rings-1)).to_a.each do |line|
     tile_size = rods[0][line]
     unless tile_size == 0
       char_offset = 0
@@ -24,11 +23,28 @@ def current_board(rods)
         board_output_string[char_offset] = "O"
         char_offset += 1
       end
+    end
 
+    tile_size = rods[1][line]
+    unless tile_size == 0
+      char_offset = 4
+      tile_size.times do
+        board_output_string[char_offset] = "O"
+        char_offset += 1
+      end
+    end
+
+    tile_size = rods[2][line]
+    unless tile_size == 0
+      char_offset = 9
+      tile_size.times do
+        board_output_string[char_offset] = "O"
+        char_offset += 1
+      end
     end
     puts board_output_string
   end
-    puts
+  puts "1   2    3"
 end
 
 def valid_move?
@@ -39,6 +55,9 @@ end
 
 
 def make_move(rods, move)
+  if move == "q"
+    exit
+  end
   move_a = move.split(",").to_a
   rod_begin = move_a[0].to_i - 1
   rod_begin_index = 0
@@ -62,7 +81,7 @@ def make_move(rods, move)
   #logical tests
   rods[rod_begin][rod_begin_index], rods[rod_end][rod_end_index] = rods[rod_end][rod_end_index], rods[rod_begin][rod_begin_index]
 #  puts "Moving from rod #{rod_begin},#{rod_begin_index} to rod #{rod_end}, #{rod_end_index}"
-  current_board(rods)
+  render(rods, num_rings)
   #end
 end
 
@@ -73,32 +92,25 @@ def play num_rings = 3
   puts "Enter where you'd like to move from and to"
   puts "in the format '1,3'. Enter 'q' to quit."
   puts
-  #puts "First enter the number of rings you'd like to play with"
+  puts "First enter the number of rings you'd like to play with"
   #initializes main variables
-  #num_rings = gets.chomp.to_i
+  num_rings = gets.chomp.to_i
   rods = [(1..num_rings).to_a, [], []]
   num_rings.times do
     rods[1] << 0
     rods[2] << 0
   end
-  current_board(rods)
-#  unless gets.chomp == "q"
-#    move = gets.chomp
-    move = "1,2"
+  move = ""
+  game_on = true
+  render(rods, num_rings)
+  while game_on == true
+    move = gets.chomp
+  #  move = "1,2"
     make_move(rods, move)
-    #delete me later:
-    make_move(rods, "1,3")
-    make_move(rods, "2,3")
-    make_move(rods, "1,2")
-    make_move(rods, "3,1")
-    make_move(rods, "3,2")
-    make_move(rods, "1,2")
-    finish_game(rods, num_rings)
-    current_board(rods)
-#  end
-
+  #  render(rods, num_rings)
+    finish_game(rods, num_rings, game_on)
+  end
+  exit
 end
-
-
 
 play 3
